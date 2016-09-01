847e2f7b1600c08d506910f18a667655057cf349
Intan Puspitasari <intanpusss@gmail.com>
2016-09-01 13:51:02 +0700

    Sample of program that using REST API to access SharePoint

---
+++ Sample - REST API/REST/REST/Features/Feature1/Feature1.feature
@@ -0,0 +1,9 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="9579c942-de7a-4962-b43a-6a5818c4d41d" description="SharePoint App Feature" featureId="9579c942-de7a-4962-b43a-6a5818c4d41d" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="REST Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="7eeedb43-a4f5-4d7d-8f56-be826f7810f2" />
+    <projectItemReference itemId="fcb99f45-b03c-4c72-8e34-3c26e3d57176" />
+    <projectItemReference itemId="d3bea7a6-1a82-4c38-ae8f-fef854516ebf" />
+    <projectItemReference itemId="645af49c-dae9-495c-b9be-b7f8ba840ad6" />
+  </projectItems>
+</feature>
---
+++ Sample - REST API/REST/REST/Scripts/App.js
@@ -0,0 +1,67 @@
+﻿'use strict';
+
+var context = SP.ClientContext.get_current();
+var user = context.get_web().get_currentUser();
+
+// This code runs when the DOM is ready and creates a context object which is needed to use the SharePoint object model
+$(document).ready(function () {
+    //getUserName();
+
+    var spAppWebUrl = decodeURIComponent(getQueryStringParameter('SPAppWebUrl'));
+
+    $("#searchButton").on("click", function () {
+        var queryUrl = spAppWebUrl + "/_api/search/query?querytext='" + $("#searchTextBox").val() + "'";
+        $.ajax({ url: queryUrl, method: "GET", headers: { "Accept": "application/json; odata=verbose" }, success: onQuerySuccess, error: onQueryError });
+    });
+});
+
+function onQuerySuccess(data) {
+    var results = data.d.query.PrimaryQueryResult.RelevantResults.Table.Rows.results;
+
+    var innerHtml = '<table>';
+   
+    $.each(results, function() {
+        innerHtml += '<tr>';
+        $.each(this.Cells.results, function () {
+            innerHtml += '<td>' + this.Value + '</td>';
+        })
+        innerHtml += '</tr>';
+    });
+    //innerHtml += '</table>';
+
+    $("#resultsDiv").append(innerHtml);
+}
+
+function onQueryError(error) {
+    $("#resultsDiv").append(error.statusText)
+}
+
+//function to get a parameter value by a specific key
+function getQueryStringParameter(urlParameterKey) {
+    var params = document.URL.split('?')[1].split('&');
+    var strParams = '';
+
+    for (var i = 0; i < params.length; i = i + 1) {
+        var singleParam = params[i].split('=');
+        if (singleParam[0] == urlParameterKey)
+            return decodeURIComponent(singleParam[1]);
+    }
+
+}
+
+//// This function prepares, loads, and then executes a SharePoint query to get the current users information
+//function getUserName() {
+//    context.load(user);
+//    context.executeQueryAsync(onGetUserNameSuccess, onGetUserNameFail);
+//}
+
+//// This function is executed if the above call is successful
+//// It replaces the contents of the 'message' element with the user name
+//function onGetUserNameSuccess() {
+//    $('#message').text('Hello ' + user.get_title());
+//}
+
+//// This function is executed if the above call fails
+//function onGetUserNameFail(sender, args) {
+//    alert('Failed to get user name. Error:' + args.get_message());
+//}
