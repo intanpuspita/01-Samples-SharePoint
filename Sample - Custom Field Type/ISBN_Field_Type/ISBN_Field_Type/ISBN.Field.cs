25ad41f036c9988f51883013264dfca123a7abd0
Intan Puspitasari <intanpusss@gmail.com>
2016-09-01 13:35:16 +0700

    Sample program to create custom field type in SharePoint

---
+++ Sample - Custom Field Type/ISBN_Field_Type/ISBN_Field_Type/ISBN.Field.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.SharePoint;
+using Microsoft.SharePoint.WebControls;
+using Microsoft.SharePoint.Security;
+
+using System.Windows.Controls;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+
+//using Contoso.SharePoint.WebControls;
+using Contoso.System.Windows.Controls;
+using System;
+
+namespace Contoso.SharePoint
+{
+    class ISBNField : SPFieldText
+    {
+        public ISBNField(SPFieldCollection fields, string fieldName) : base(fields, fieldName){ }
+        public ISBNField(SPFieldCollection fields, string typeName, string displayName) : base(fields, typeName, displayName){ }
+
+        public override BaseFieldControl FieldRenderingControl
+        {
+            [SharePointPermission(SecurityAction.LinkDemand, ObjectModel = true)]
+            get
+            {
+                BaseFieldControl fieldControl = new ISBNFieldControl();
+                fieldControl.FieldName = this.InternalName;
+
+                return fieldControl;
+            }
+        }
+
+        public override string GetValidatedString(object value)
+        {
+            if ((this.Required == true)
+               &&
+               ((value == null)
+                ||
+               ((String)value == "")))
+            {
+                throw new SPFieldValidationException(this.Title
+                    + " must have a value.");
+            }
+            else
+            {
+                ISBN10ValidationRule rule = new ISBN10ValidationRule();
+                ValidationResult result = rule.Validate(value, CultureInfo.InvariantCulture);
+
+                if (!result.IsValid)
+                {
+                    throw new SPFieldValidationException((String)result.ErrorContent);
+                }
+                else
+                {
+                    return base.GetValidatedString(value);
+                }
+            }
+        }
+    }
+}
---
+++ Sample - Custom Field Type/ISBN_Field_Type/ISBN_Field_Type/ISBN.FieldControl.cs
@@ -0,0 +1,100 @@
+﻿using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Runtime.InteropServices;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.WebControls;
+using System;
+
+namespace Contoso.SharePoint
+{
+    class ISBNFieldControl : TextField
+    {
+        protected Label ISBNPrefix;
+        protected Label ISBNValueForDisplay;
+
+        protected override string DefaultTemplateName
+        {
+            get
+            {
+                if (this.ControlMode == SPControlMode.Display)
+                {
+                    return this.DisplayTemplateName;
+                }
+                else
+                {
+                    return "ISBNFieldControl";
+                }
+            }
+        }
+
+        public override string DisplayTemplateName
+        {
+            get
+            {
+                return "ISBNFieldControlForDisplay";
+            }
+            set
+            {
+                base.DisplayTemplateName = value;
+            }
+        }
+
+        protected override void CreateChildControls()
+        {
+            if (this.Field != null)
+            {
+                // Make sure inherited child controls are completely rendered.
+                base.CreateChildControls();
+
+                // Associate child controls in the .ascx file with the 
+                // fields allocated by this control.
+                this.ISBNPrefix = (Label)TemplateContainer.FindControl("ISBNPrefix");
+                this.textBox = (TextBox)TemplateContainer.FindControl("TextField");
+                this.ISBNValueForDisplay = (Label)TemplateContainer.FindControl("ISBNValueForDisplay");
+
+                if (this.ControlMode != SPControlMode.Display)
+                {
+                    if (!this.Page.IsPostBack)
+                    {
+                        if (this.ControlMode == SPControlMode.New)
+                        {
+                            textBox.Text = "0-000-00000-0";
+
+                        } // end assign default value in New mode
+
+                    }// end if this is not a postback 
+
+                    // Do not reinitialize on a postback.
+
+                }// end if control mode is not Display
+                else // control mode is Display 
+                {
+                    // Assign current value from database to the label control
+                    ISBNValueForDisplay.Text = (String)this.ItemFieldValue;
+
+                }// end control mode is Display
+
+            }// end if there is a non-null underlying ISBNField 
+
+            // Do nothing if the ISBNField is null.
+        }
+
+        public override object Value
+        {
+            get
+            {
+                EnsureChildControls();
+                return base.Value;
+            }
+            set
+            {
+                EnsureChildControls();
+                base.Value = (String)value;
+                // The value of the ISBNPrefix field is hardcoded in the
+                // template, so it is not set here.
+            }
+        }
+    }
+}
